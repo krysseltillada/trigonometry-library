e00f34c985a71fd48c964bd883190080bfc23c07
kryssel tillada <krysseltillada@gmail.com>
2015-06-10 06:34:24 -0700

    added some new functions
    tri::tan_deg(int, double) --> finds the missing value which is(opposite);
    tri::sin_deg(int, double) --> finds the missing value which is (opposite);
    tri::cos_deg(int, double) --> finds the missing value which is (adjacent);
    
    tri::sine(double) --> returns the sine of the argument value of double
    tri::cosine(double) --> returns the cosine of the argument value of double
    tri::tangent(double) --> returns the tangent of the argument value of double

--- trig_lib.h
+++ trig_lib.h
@@ -6,16 +6,40 @@
 #include <sstream>
 
 namespace tri {
-    const double PI = 3.14;
+    const double PI = 3.14159265359;
 
-    double get_rad(double r) {
+    double tan_deg (int _deg, double adj) {
+        return (tan((_deg * PI) / 180) * adj);
+    }
+
+    double sin_deg (int _deg, double hyp) {
+        return (sin((_deg * PI) / 180) * hyp);
+    }
+
+    double cos_deg (int _deg, double hyp) {
+        return (cos((_deg * PI) / 180) * hyp);
+    }
+
+    double sine (double deg) {
+        return (sin((deg * PI) / 180));
+    }
+
+    double cosine (double deg) {
+        return (cos((deg * PI) / 180));
+    }
+
+    double tangent (double deg) {
+        return (tan((deg * PI) / 180));
+    }
+
+    double get_rad (double r) {
         std::ostringstream strs;
         strs << r;
         std::string s = strs.str();
         auto i = 0u;
-        while(i != s.size()) {
-            if(s[i] == '.') {
-                return atof(s.c_str());
+        while (i != s.size()) {
+            if (s[i] == '.') {
+                return atof (s.c_str());
             }
             else {
                 i++;
@@ -24,50 +48,50 @@ namespace tri {
         return 0.0;
     }
 
-    double deg_to_rad(int d) {
+    double deg_to_rad (int d) {
         auto rad = 0.0;
         rad = d * (PI / 180);
         return rad;
     }
 
-    int rad_to_deg(double r) {
+    int rad_to_deg (double r) {
         auto degrees = 0;
         degrees = r * 180 / PI;
         return degrees;
     }
 
-    int get_degree(double t_s) {
+    int get_degree (double t_s) {
         std::ostringstream strs2;
         strs2 << t_s;
         std::string s1 = strs2.str();
         auto i = 0u;
-        while(i != s1.size()) {
-            if(s1[i] == '.') {
+        while (i != s1.size()) {
+            if (s1[i] == '.') {
                 return 0;
             }
             i++;
         }
-        return atoi(s1.c_str());
+        return atoi (s1.c_str());
     }
 
-    int ref_angle(double r_a) {
+    int ref_angle (double r_a) {
         auto ref_angle_ = 0, excd_ref_angle_ = 0, temp_ref_angle_1 = 0;
 
-        while(true) {
-        if(r_a > 0){
-        if(r_a <= 90) {
+        while (true) {
+        if (r_a > 0){
+        if (r_a <= 90) {
            ref_angle_ = r_a;
            return ref_angle_;
         }
-        else if(r_a <= 180) {
+        else if (r_a <= 180) {
             ref_angle_ = 180 - r_a;
             return ref_angle_;
         }
-        else if(r_a <= 270) {
+        else if (r_a <= 270) {
             ref_angle_ = r_a - 180;
             return ref_angle_;
         }
-        else if(r_a <= 360) {
+        else if (r_a <= 360) {
             ref_angle_ = 360 - r_a;
             return ref_angle_;
         } else {
@@ -78,19 +102,19 @@ namespace tri {
         }
         }
         else {
-            if(r_a >= -90) {
+            if (r_a >= -90) {
            ref_angle_ = r_a  * - 1;
            return ref_angle_;
         }
-        else if(r_a >= -180) {
+        else if (r_a >= -180) {
             ref_angle_ = r_a + 180;
             return ref_angle_;
         }
-        else if(r_a >= -270) {
+        else if (r_a >= -270) {
             ref_angle_ = 180 + r_a;
             return ref_angle_ * - 1;
         }
-        else if(r_a >= -360) {
+        else if (r_a >= -360) {
             ref_angle_ = r_a + 360;
             return ref_angle_;
         } else {
